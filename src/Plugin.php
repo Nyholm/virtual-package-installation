5160ab5024647dfb63deb1b563038b53303c5471
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-01-06 10:12:59 +0100

    test

--- src/Plugin.php
+++ src/Plugin.php
@@ -3,14 +3,40 @@
 namespace Nyholm\VirtualPackageInstallation;
 
 use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Installer\PackageEvent;
+use Composer\Installer\PackageEvents;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
-class Plugin extends PluginInterface
+class Plugin implements PluginInterface, EventSubscriberInterface
 {
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            PackageEvents::POST_PACKAGE_INSTALL => 'listen',
+            PackageEvents::POST_PACKAGE_UPDATE => 'listen',
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function activate(Composer $composer, IOInterface $io)
     {
-        $installer = new TemplateInstaller($io, $composer);
-        $composer->getInstallationManager()->addInstaller($installer);
+        $composer->getEventDispatcher()->addSubscriber($this);
+    }
+
+    /**
+     * Listens to Composer events.
+     *
+     * @param PackageEvent $event The Composer event.
+     */
+    public function listen(PackageEvent $event)
+    {
+        $foo = 'bar';
     }
 }
